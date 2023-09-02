47b36b6236b00beb57a8f6094464ca4c4e254026
RageNeko26 <rneko2006@gmail.com>
2023-09-02 20:26:12 +0700

    feat: Initialize connection

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+
+	if err != nil {
+		fmt.Println(err)
+		panic(1)
+	}
+
+	defer conn.Close()
+
+	fmt.Println("Connection to RabbitMQ is success!")
+
+}
