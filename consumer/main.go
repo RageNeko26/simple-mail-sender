9196ac121ee0c6608e3fe427422e385df563cb1d
RageNeko26 <rneko2006@gmail.com>
2023-09-02 21:20:17 +0700

    feat: Intialize connection RabbitMQ

---
+++ consumer/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+	"fmt"
+)
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672")
+	
+	if err != nil {
+		fmt.Println("Failed to connect instance RabbitMQ")
+	}
+
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+
+	if err != nil {
+		fmt.Println("Failed to open channel!")
+	}
+
+	defer ch.Close()
+
+	msg, err := ch.Consume(
+		"TestQueue",
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msg {
+			fmt.Println("Received message:", d.Body)
+		}
+	}()
+
+	fmt.Println("Successfully connect to RabbitMQ")
+	fmt.Println("Waiting for message...")
+
+	<-forever
+
+	
+}
